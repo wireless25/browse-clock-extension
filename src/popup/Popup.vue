2d4e5af0e463e0024faa332cbfb2d9e00a2eb890
Stephan Simonett <wireless25@me.com>
2025-07-23 22:27:57 +0200

    feat: support dark mode in popup

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
+import { applyTheme, prefersDarkMode } from '~/logic/common-setup'
+
 function openOptionsPage() {
   browser.runtime.openOptionsPage()
 }
+
+// Initial check
+applyTheme(prefersDarkMode)
+
+// Listen for changes
+prefersDarkMode.addEventListener('change', mql => applyTheme(mql))
 </script>
 
 <template>
-  <main class="w-[300px] px-4 py-5 text-center text-gray-700">
+  <main class="w-[300px] px-4 py-5 text-center text-gray-700 dark:bg-dark dark:text-gray-100">
     <Logo />
-    <div>Popup</div>
-
-    <button class="btn mt-2" @click="openOptionsPage">
+    <h1 class="text-2xl font-bold">
+      BrowseClock
+    </h1>
+    <button class="btn mt-4" @click="openOptionsPage">
       Open Options
     </button>
   </main>
